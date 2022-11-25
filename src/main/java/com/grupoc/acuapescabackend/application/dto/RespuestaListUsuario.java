0d27c62b4ec76f66a81b9e791fcf1d40db3dd9f6
Angie Pizarro <apizarroc2@upao.edu.pe>
2022-11-25 15:24:09 -0500

    RespuestaListUsuario

--- src/main/java/com/grupoc/acuapescabackend/application/dto/RespuestaListUsuario.java
+++ src/main/java/com/grupoc/acuapescabackend/application/dto/RespuestaListUsuario.java
@@ -1,4 +1,16 @@
 package com.grupoc.acuapescabackend.application.dto;
 
-public class RespuestaListUsuario {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.grupoc.acuapescabackend.domain.entities.Usuario;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class RespuestaListUsuario extends Respuesta{
+
+    @JsonProperty("dato")
+    List<Usuario> data;
 }
