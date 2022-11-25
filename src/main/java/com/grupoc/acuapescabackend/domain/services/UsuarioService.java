4e1377a3a0ab556e92ec246d86a819d10df71527
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-11-25 15:16:06 -0500

    chore: add modificarUsuario and eliminarUsuario on the services

--- src/main/java/com/grupoc/acuapescabackend/domain/services/UsuarioService.java
+++ src/main/java/com/grupoc/acuapescabackend/domain/services/UsuarioService.java
@@ -7,11 +7,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
 
     public Usuario crearUsuario(Usuario usuario) {
         return usuarioRepository.save(usuario);
+    }
+
+    public Usuario modificarUsuario(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void eliminarUsuario(Integer idUsuario) {
+        usuarioRepository.deleteById(idUsuario);
     }
 }
